c98849d75508bf0643bb45f53f437f8d78a95e46
haroldo sales <haroldognu@gmail.com>
2021-11-20 09:03:50 -0300

    destruturação

---
+++ 02-Javascript/modulo7/aula01/script.js
@@ -0,0 +1,86 @@
+let nomes = ['jose','couves'];
+
+let [prenomes, sobrenomes] = nomes;
+
+console.log(prenomes);
+console.log(sobrenomes);
+
+/*function soma() {
+  let valores = [...arguments]
+
+  return valores.reduce(
+    (total, i) => (total += i)
+  );
+}
+
+console.log(soma(1, 2, 3, 4));
+output.innerHTML = "";
+let A;
+let x = -10;
+try {
+  if (x < 0) throw new Error("valor Não pode ser negativo");
+  A = x;
+  output.innerHTML = `<p>A = ${A}</p>`;
+
+} catch (erro) {
+  output.innerHTML = `<p>${erro.message}</p>`;
+  console.log(erro.name);
+  console.log(erro.message);
+  console.log(erro.stack);
+} finally{
+  console.log("fim");
+}
+let obj1 = { nome:  'Jose Couves', idade: 25};
+
+let obj2 = obj1;
+
+let clone1 = {...obj1} ;
+
+obj1.nome = 'Maria Souza';
+console.log(clone1.nome);
+console.log(obj1.nome);
+
+
+
+
+let mat = { matricula: 1234, curso: 'web'};
+
+let  aluno = { ...obj1,...mat};
+
+console.log(aluno);
+
+let funcionario = {...obj1, slaario:3400};
+
+console.log(funcionario);
+let obj1 = { nome:  'Jose Couves', idade: 25};
+
+let obj2 = obj1;
+
+let clone1 = {...obj1} ;
+
+obj1.nome = 'Maria Souza';
+console.log(clone1.nome);
+console.log(obj1.nome);
+
+
+
+
+let mat = { matricula: 1234, curso: 'web'};
+
+let  aluno = { ...obj1,...mat};
+
+console.log(aluno);
+
+let funcionario = {...obj1, slaario:3400};
+
+console.log(funcionario);
+
+*/
+///7.4 desstrução
+
+//let numeros = [1,2,3,4];
+
+//let maisNumeros = [... numeros,5,6];
+
+
+//console.log(maisNumeros);
