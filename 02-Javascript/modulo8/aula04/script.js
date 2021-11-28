b30b71d314e936b93fa152a7acae044268408709
haroldo sales <haroldognu@gmail.com>
2021-11-28 19:43:07 -0300

    update

--- 02-Javascript/modulo8/aula04/script.js
+++ 02-Javascript/modulo8/aula04/script.js
@@ -1,19 +1,39 @@
 //função construtora
 
-function Forma(x, y) {
-  this.x = x;
-  this.y = y;
+class Forma {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+  move(dx, dy){
+    this.x += dx;
+    this.y += dy;
+  }
+};
+
+
+
+class Retangulo extends Forma{
+  constructor (x, y, l, a, n) {
+    super(x, y);
+    this.largura = l;
+    this.altura = a;
+}
+
 }
 
 
-let f1 = new Forma(10, 15);
 
-console.log(f1);
+ let r1 = new Retangulo(10, 15, 100, 50);
 
-
-let Forma2 = {
-  x: 10,
-  y: 15
-}
-
-console.log(Forma2);
+ r1.move(15,15);
+ console.log(r1);
+//
+// Forma.prototype.move = function(dx, dy) {
+//
+// };
+// Retangulo.prototype = Object.create(Forma.prototype);
+// Retangulo.prototype.constructor = Retangulo;
+//f1.move(15, 15);
+// console.log(f1);
+//console.log(r1);
