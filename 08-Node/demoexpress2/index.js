4ebf5985b5307a0c61009da0a51e6a63e00ccec3
haroldosales <haroldognu@gmail.com>
2022-06-12 21:51:13 -0300

    midlle

--- 08-Node/demoexpress2/index.js
+++ 08-Node/demoexpress2/index.js
@@ -1,19 +1,28 @@
 // criando  api 
 const express = require('express');
 const app = express();
-const port = 3030;
+const port = 3000;
+
+const logMiddleeware = function (req, res, next) {
+   console.log("Api recebeu alguma coisa");
+
+   next();
+};
+
+app.use(express.static("./site"));
+
+app.use(logMiddleeware);
 
 app.get("/", function (req, res) {
+   console.log("Hello Wordl");
    res.send("Hello World");
 });
+
 
 app.get("/pt", function (req, res) {
    res.send("Ola Mundo");
 });
 
-app.post("/post", function (req, res) {
-   res.send({ sestatus: "ok" });
-})
 
 
 app.listen(port, () => {
