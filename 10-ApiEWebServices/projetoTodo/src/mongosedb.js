ef2d7f234be0ff228474feaa2cf92f44d0c52d1c
haroldosales <haroldognu@gmail.com>
2022-07-12 17:19:07 -0300

    update

--- 10-ApiEWebServices/projetoTodo/src/mongosedb.js
+++ 10-ApiEWebServices/projetoTodo/src/mongosedb.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 module.exports = {
     init: () => {
         mongoose.connect(
-          `mongodb+srv://admin:toor1234@cluster0.kzbqy.mongodb.net/nodeTodo?retryWrites=true&w=majority`,
+          `mongodb+srv://XXX:XXX@clxr0.xxx.mongodb.net/nodeTodo?retryWrites=true&w=majority`,
                 )
         .then((res) => console.log(`Connection Succesful ${res}`))
         .catch((err) => console.log(`Error in DB connection ${err}`));
@@ -15,19 +15,3 @@ module.exports = {
 
 
 
-
-
-
-/* module.exports = {
-    init: () => {
-        mongo.connect(`mongodb+srv://${user}:${pass}@cluster0.7u6nt.mongodb.net/${database}?retryWrites=true&w=majority´,
-        {
-            useNewUrlParser: true,
-            useUnitfiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-        }
-        ).then((res) => console.log("Connectou com sucesso"))
-        .catch((err) => console.log("error db connecter"));
-    },
-} */
