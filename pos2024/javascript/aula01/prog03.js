2033acb3fcfefe6e3e84f993dfc46c3c21d56b6b
haroldosales <haroldognu@gmail.com>
2024-05-01 15:30:06 -0300

    ajuste das paginas

--- pos2024/javascript/aula01/prog03.js
+++ pos2024/javascript/aula01/prog03.js
@@ -5,3 +5,7 @@ console.log(1, n, typeof n) ;
 let a = 3;
 //decremento
 console.log(3, a--*2,a);
+
+//Concetamação de string
+//coerção
+ 
---
+++ pos2024/javascript/aula01/prog04.js
@@ -0,0 +1,11 @@
+let idade = 12;
+let acesso;
+
+if (idade >= 18) {
+  acesso = true;
+} else {
+  acesso = false;
+}
+//acesso = idade >= 18 ? true : false
+
+console.log(acesso);
