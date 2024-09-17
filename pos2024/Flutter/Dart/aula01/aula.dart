1ad11665515f87922ebbc1ad90ad607f6e0da16c
haroldosales <haroldognu@gmail.com>
2024-09-17 19:22:48 -0300

    init dart

---
+++ pos2024/Flutter/Dart/aula01/aula.dart
@@ -0,0 +1,18 @@
+var myText = "meu text";
+String myOtherText = "outra maneira";
+
+int myInt =5;
+var myNumber = 5.9;
+
+var myArray = [1,2,3,4];
+Object myObject = {"key": "value"};
+
+
+
+
+void main(){
+
+  
+  print("ola mundo");
+  print(myText + myOtherText );
+}
